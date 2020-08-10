98e6f86c131557ee2b97a15233af23ab1f45824d
tetsu5555 <tetete1118@gmail.com>
2020-08-10 12:27:41 +0900

    echoparams.sh

---
+++ chapter2/echoparams.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Cybersecurity Ops with bash
+# echoparams.sh
+#
+# Description:
+# Demonstrates accessing parameter in bash
+#
+# Usage:
+# ./echoparams.sh <param 1> <param 2> <param 3>
+
+echo $#
+echo $0
+echo $1
+echo $2
+echo $3
